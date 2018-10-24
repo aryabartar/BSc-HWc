65ad400f7bd3fdfac69c490433d71ba7a732c53c
Arya Khaligh <a.khaligh@aut.ac.ir>
2018-10-24 07:51:29 +0330

    Para class created.

--- DS/codes/HW1/03.py
+++ DS/codes/HW1/03.py
@@ -1,1 +1,15 @@
-git
+
+
+
+
+string_len = input('')
+string = input('')
+array = []
+
+for i in range(0, int(string_len)):
+    print(string[i])
+
+class Para:
+    def __init__(self):
+        self.value = "("
+        self.index = 0
