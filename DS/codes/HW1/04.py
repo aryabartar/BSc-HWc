b2a60fec4a81299bdb04607a3d4a1d863105ba99
Arya Khaligh <a.khaligh@aut.ac.ir>
2018-11-02 07:07:53 +0330

    Final change is made.

--- DS/codes/HW1/04.py
+++ DS/codes/HW1/04.py
@@ -6,12 +6,12 @@ weights = [int(i) for i in weights]
 weights.sort()
 
 counter = 0
-pointer = q_info[0] - 1
 
 i = 0
 t = len(weights) - 1
 
 while i <= t:
+    counter += 1
 
     if weights[i] + weights[t] <= q_info[1]:
         i += 1
@@ -19,7 +19,5 @@ while i <= t:
 
     else:
         t -= 1
-
-    counter += 1
 
 print(counter)
