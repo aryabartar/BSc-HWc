4fb7c06b970376680916baa0f5c254cb529b8252
Arya Khaligh <a.khaligh@aut.ac.ir>
2018-11-02 07:06:16 +0330

    Wow, Finally accepted!

--- DS/codes/HW1/04.py
+++ DS/codes/HW1/04.py
@@ -6,29 +6,20 @@ weights = [int(i) for i in weights]
 weights.sort()
 
 counter = 0
-pointer = q_info[0]-1
+pointer = q_info[0] - 1
 
-for i in range(0, int(q_info[0])):
-    # print ("&&&First if (i) running i is : " + str(i))
-    # print("&&&Pointer is : " + str(pointer))
-    if pointer == i :
-        counter += 1
-        break
-    for j in range(pointer, i-1 , -1):
-        # print("========")
-        # print("i is : " + str(i))
-        # print("j is : " + str(j))
-        # print("pointer is : " + str(pointer))
-        # print("Counter is : " + str(counter))
-        if weights[i] + weights[j] <= q_info[1]:
-            # print("First if run.")
-            counter += 1
-            pointer = j-1
-            # print("========")
-            break
-        else:
-            # print("Else run")
-            counter += 1
-            # print("========")
+i = 0
+t = len(weights) - 1
+
+while i <= t:
+
+    if weights[i] + weights[t] <= q_info[1]:
+        i += 1
+        t -= 1
+
+    else:
+        t -= 1
+
+    counter += 1
 
 print(counter)
