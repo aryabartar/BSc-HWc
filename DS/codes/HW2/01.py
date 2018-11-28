4de7bf15f9a851c5b4a3e17a4b672d6096afb7ff
arya <a.khaligh@aut.ac.ir>
2018-11-28 20:40:12 +0330

    insert_max_heap defined.

--- DS/codes/HW2/01.py
+++ DS/codes/HW2/01.py
@@ -1,11 +1,11 @@
-def max_heapify(array, i):
+def max_heapify(array, i, heap_size):
     max = i
-    if array[i] < array[2 * i]:
-        max = 2 * i
-        print("111")
-    if array[max] < array[2 * i + 1]:
-        max = 2 * i + 1
-        print("222")
+    if 2 * i < heap_size:
+        if array[i] < array[2 * i]:
+            max = 2 * i
+    if 2 * i + 1 < heap_size:
+        if array[max] < array[2 * i + 1]:
+            max = 2 * i + 1
     if not max == i:
         temp = array[max]
         array[max] = array[i]
@@ -14,8 +14,13 @@ def max_heapify(array, i):
     return array
 
 
-def input_max_heap(number, max_heap_array):
+def insert_max_heap(number, max_heap_array):
     max_heap_array.append(number)
+    i = int(len(max_heap_array) / 2)
+    while i > 0:
+        max_heapify(max_heap_array, i, len(max_heap_array))
+        i = int(i / 2)
+    print(max_heap_array)
 
 
 max_heap_array = [0]
@@ -23,7 +28,7 @@ for_number = input("")
 for i in range(0, int(for_number)):
     user_input = input("").split(" ")
     if user_input[0] == "1":
-        input_max_heap(int(user_input[1]), max_heap_array)
+        insert_max_heap(int(user_input[1]), max_heap_array)
     else:
         pass
     print(max_heap_array)
