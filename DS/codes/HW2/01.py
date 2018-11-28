d1aab647c5850429b9c7082be75bf8e2b91d06bd
arya <a.khaligh@aut.ac.ir>
2018-11-28 21:55:06 +0330

    sort_heap added.

--- DS/codes/HW2/01.py
+++ DS/codes/HW2/01.py
@@ -1,8 +1,12 @@
-def max_heapify(array, i):
+def max_heapify(array, i, heap_size=None):
     # print("========")
     # print(array)
+    # print("========")
+
     # print(" i = " + str(i))
-    heap_size = len(array)
+    if heap_size is None:
+        heap_size = len(array)
+
     max = i
     if 2 * i < heap_size:
         # print("Can check first if")
@@ -19,34 +23,42 @@ def max_heapify(array, i):
         temp = array[max]
         array[max] = array[i]
         array[i] = temp
-        max_heapify(array, max)
+        max_heapify(array, max , heap_size)
     # print("======")
 
 
 def build_max_heap(heap_array):
     for i in reversed(range(1, int(len(heap_array) / 2) + 1)):
-        print(i)
         max_heapify(heap_array, i)
-        print(heap_array)
-    print(heap_array)
 
 
 def insert_heap(number, heap_array):
     heap_array.append(number)
 
 
-#
-# heap_array = [0]
-# for_number = input("")
-# for i in range(0, int(for_number)):
-#     user_input = input("").split(" ")
-#     if user_input[0] == "1":
-#         insert_heap(int(user_input[1]), heap_array)
-#     else:
-#         print(heap_array)
-#         # return_index = len(heap_array) - int(len(max_heap_array) / 3)
-#         # print(return_index)
-#         # print(max_heap_array[return_index])
-#     # print(max_heap_array)
+def sort_heap(heap_array):
+    build_max_heap(heap_array)
+    # print(heap_array)
 
-build_max_heap([0, 1, 7, 9, 21, 8, 5, 9])
+    for i in reversed(range(2, len(heap_array))):
+        # print(i)
+        heap_array[1], heap_array[i] = heap_array[i], heap_array[1]
+        max_heapify(heap_array, 1, i)
+        # print(heap_array)
+
+    # print(heap_array)
+
+    # for_number = input("")
+    # for i in range(0, int(for_number)):
+    #     user_input = input("").split(" ")
+    #     if user_input[0] == "1":
+    #         insert_heap(int(user_input[1]), heap_array)
+    #     else:
+    #         print(heap_array)
+    #         # return_index = len(heap_array) - int(len(max_heap_array) / 3)
+    #         # print(return_index)
+    #         # print(max_heap_array[return_index])
+    #     # print(max_heap_array)
+
+
+sort_heap([0, 1, 7, 9, 21, 8, 5, 9])
