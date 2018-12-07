dc56e4b263fd11cb18e7e20e2d7ab0494640957b
arya <a.khaligh@aut.ac.ir>
2018-12-07 12:57:11 +0330

    Return vertexes added.

--- DS/codes/HW2/03.py
+++ DS/codes/HW2/03.py
@@ -1,18 +1,23 @@
-number_of_v = int(input(""))
-edges = {}
+def get_edges_and_vertex():
+    number_of_v = int(input(""))
+    edges = {}
+    vertexes = []
 
-for i in range(1, number_of_v + 1):
-    edges[i] = []
+    for i in range(1, number_of_v + 1):
+        edges[i] = []
+        vertexes.append(i)
 
-for i in range(1, number_of_v):
-    edge = input('').split(' ')
-    edge[0] = int(edge[0])
-    edge[1] = int(edge[1])
+    for i in range(1, number_of_v):
+        edge = input('').split(' ')
+        edge[0] = int(edge[0])
+        edge[1] = int(edge[1])
 
-    edge_tuple = (edge[0], edge[1])
-    edges[edge[0]].append(edge[1])
-    edges[edge[1]].append(edge[0])
+        edge_tuple = (edge[0], edge[1])
+        edges[edge[0]].append(edge[1])
+        edges[edge[1]].append(edge[0])
 
-print(edges)
+    return edges, vertexes
 
+
+edges, vertexes = get_edges_and_vertex()
 
