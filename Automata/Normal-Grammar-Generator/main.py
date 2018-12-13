6ab5016f97e714124a9aa32a290d7c8358d032d1
arya <a.khaligh@aut.ac.ir>
2018-12-13 16:57:26 +0330

    add remove_l_production

--- Automata/Normal-Grammar-Generator/main.py
+++ Automata/Normal-Grammar-Generator/main.py
@@ -1,5 +1,5 @@
 def read_files():
-    text_file = open("test-cases/1.txt", "r").read()
+    text_file = open("test-cases/0.txt", "r").read()
     lines = text_file.split("\n")
 
     dict = {}
@@ -13,4 +13,31 @@ def read_files():
     return dict
 
 
-print(read_files())
+def remove_l_production(dict):
+    v = []
+    for key in dict:
+        for item in dict[key]:
+            if item == "l":
+                v.append(key)
+                break
+    # TODO : should you repeate this multiple times?!
+    for key in dict:
+        if key in v:
+            continue
+
+        for item in dict[key]:
+            is_nullable = True
+            for char in item:
+                if char not in v:
+                    is_nullable = False
+
+            if is_nullable:
+                v.append(key)
+                break
+
+    return dict
+
+
+dict = read_files()
+print(dict)
+dict = remove_l_production(dict)
