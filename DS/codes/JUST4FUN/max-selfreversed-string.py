9d34a771bfdd6dd0f1f8e6a561594fb532a4629b
arya <a.khaligh@aut.ac.ir>
2018-12-24 10:20:31 +0330

    Add find_max_reverse.

---
+++ DS/codes/JUST4FUN/max-selfreversed-string.py
@@ -0,0 +1,32 @@
+def can_reverse(A, index, length):
+    try:
+        if index - length < 0 or index + length > len(A):
+            return length
+        elif A[index - length] == A[index + length]:
+            # print("len is : ", length, " | A[index - length] is : ",
+            #       A[index - length], " | A[index + length] is : ",
+            #       A[index + length])
+            length += 1
+            return can_reverse(A, index, length)
+        else:
+            return length
+    except:
+        return length
+
+
+def find_max_reverse(A):
+    index = 0
+    max_reverse_len = 0
+    for i in A:
+        reverse_len = can_reverse(A, index, 0)
+        index += 1
+        if reverse_len > max_reverse_len:
+            max_reverse_len = reverse_len
+        # print("i is : ", i, " | ", "len is : ", reverse_len)
+
+    if max_reverse_len == 0:
+        return 0
+    return max_reverse_len * 2 - 1
+
+
+print(find_max_reverse("aakayakbaab"))  # PRINTS 5
