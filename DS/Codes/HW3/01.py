d59cce7343b47e0ff98a052ef84aea1aeaea72a6
arya <a.khaligh@aut.ac.ir>
2019-01-15 20:42:24 +0330

    Add 02.py

--- DS/Codes/HW3/01.py
+++ DS/Codes/HW3/01.py
@@ -1,10 +1,18 @@
+def divide(weights):
+    for i in reversed(range(0, len(weights))):
+        
+        for j in reversed(range(0, i)):
+            print(weights[j])
+        print(" \n ")
+
+
 def get_weights():
-    input()  # We don't need n
-    weights = input()
+    # input()  # We don't need n
+    weights = [1, 2, 5, 3, 4, 5]
     weights_array = []
 
-    for c in weights :
-        if c != " " :
+    for c in weights:
+        if c != " ":
             weights_array += [int(c)]
 
     weights_array.sort()
@@ -12,4 +20,5 @@ def get_weights():
     return weights_array
 
 
-print(get_weights())
+weights = get_weights()
+divide(weights)
