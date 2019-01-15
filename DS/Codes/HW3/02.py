3f4997ed32a9b10197362a1aeb4db779c04f0d3a
arya <a.khaligh@aut.ac.ir>
2019-01-15 22:57:07 +0330

    Modified 02.py

--- DS/Codes/HW3/02.py
+++ DS/Codes/HW3/02.py
@@ -1,21 +1,44 @@
+def compare_strings(str1, str2):
+    if str1 > str2:
+        return str1
+    return str2
+
+
 def find_min_insertions(str, l, h):
+    print("------")
+    print(l, " ", h)
 
     if l > h:
-        return 1000000
+        return ""
     if l == h:
-        return 0
+        return str[l]
     if l == h - 1:
         if str[l] == str[h]:
-            return 0
+            return str[l:h + 1]
         else:
-            return 1
+            print(str[h] + str[l:h + 1], "\n")
+            return str[h] + str[l:h + 1]
 
     if str[l] == str[h]:
-        return find_min_insertions(str, l + 1, h - 1)
+        print("kjdifjiejfiejf")
+        print(find_min_insertions(str, l + 1, h - 1))
+        return str[l] + find_min_insertions(str, l + 1, h - 1) + str[l]
     else:
-        return (min(find_min_insertions(str, l, h - 1),
-                    find_min_insertions(str, l + 1, h)) + 1)
+        str1 = find_min_insertions(str, l, h - 1)
+        str2 = find_min_insertions(str, l + 1, h)
+        print("str1 is : ", str1)
+        print("str2 is : ", str2)
+
+        if len(str1) >= len(str2):
+            temp_str = str[h] + str1 + str[h]
+            print("1 is chosen : " + temp_str)
+            return temp_str
+
+        else:
+            temp_str = str[l] + str2 + str[l]
+            print("2 is chosen : " + temp_str)
+            return temp_str
 
 
-str = "geeks"
+str = "MADAMIMADAM"
 print(find_min_insertions(str, 0, len(str) - 1))
