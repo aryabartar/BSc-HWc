dbe1707b61b18c8039d272ce28ef03ececb5f60a
arya <a.khaligh@aut.ac.ir>
2019-01-16 00:20:21 +0330

    Remove unnecessary print comments

--- DS/Codes/HW3/02.py
+++ DS/Codes/HW3/02.py
@@ -5,8 +5,6 @@ def compare_strings(str1, str2):
 
 
 def find_min_insertions(matrix, str, l, h):
-    # print("------")
-    # print(l, " ", h)
 
     if l > h:
         return ""
@@ -29,32 +27,24 @@ def find_min_insertions(matrix, str, l, h):
         return matrix[l][h]
 
     if str[l] == str[h]:
-        # print("OOOOOOPPPPSSS")
         matrix[l][h] = str[l] + find_min_insertions(matrix, str, l + 1, h - 1) + str[l]
         return matrix[l][h]
     else:
         str1 = find_min_insertions(matrix, str, l, h - 1)
         str2 = find_min_insertions(matrix, str, l + 1, h)
-        # print("str1 is : ", str1)
-        # print("str2 is : ", str2)
 
         temp_str1 = str[h] + str1 + str[h]
         temp_str2 = str[l] + str2 + str[l]
         if len(str1) < len(str2):
-            # print("1 is chosen : " + temp_str1)
             return temp_str1
 
         elif len(str1) > len(str2):
-            # print("2 is chosen : " + temp_str2)
             return temp_str2
 
         else:
-            # print("EQUALITY!!!")
             if temp_str1 == compare_strings(temp_str1, temp_str2):
-                # print("2 is chosen : " + temp_str1)
                 return temp_str1
             else:
-                # print("2 is chosen : " + temp_str2)
                 return temp_str2
 
 
@@ -62,5 +52,4 @@ str = input()
 w = len(str)
 matrix = [[0 for x in range(w)] for y in range(w)]
 print(find_min_insertions(matrix, str, 0, len(str) - 1))
-# print(matrix)
-# print(compare_strings("A" , "B"))
+
