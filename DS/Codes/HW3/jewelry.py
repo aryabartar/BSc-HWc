e54807528b69142400d7e1ff913d82fa5d33bb7b
arya <a.khaligh@aut.ac.ir>
2019-01-16 22:02:23 +0330

    Add B function

--- DS/Codes/HW3/jewelry.py
+++ DS/Codes/HW3/jewelry.py
@@ -1,5 +1,25 @@
+def B(weights, sum1):
+    def mask(lst, m):
+        m = m.zfill(len(lst))
+        return map(lambda x: x[0], filter(lambda x: x[1] != '0', zip(lst, m)))
+
+    def subset_sum(lst, target):
+        for i in range(2 ** len(lst)):
+            pick = mask(lst, bin(i)[2:])
+            if sum(pick) == target:
+                yield pick
+
+    return len(list(subset_sum(weights, sum1)))
+
+
+
 def divide(weights):
-    print(weights)
+    max_s = 3000000
+    res = 0
+
+    for i in range(0, len(weights)):
+        for s in range(weights[i], max_s):
+            res += B(i, s - weights[i], weights) * F(len(weights) - 1 - i, s, weights)
 
 
 def get_weights():
@@ -10,5 +30,8 @@ def get_weights():
     return weights
 
 
-weights = get_weights()
-divide(weights)
+# weights = get_weights()
+# divide(weights)
+# print(B([1, 2, 3, 4], 3, 5))
+
+
