57f72812fe376a4ebcd5db82ee54f9d16edef422
arya <a.khaligh@aut.ac.ir>
2019-01-16 12:53:31 +0330

    Add Price class .

--- DS/Codes/HW3/03.py
+++ DS/Codes/HW3/03.py
@@ -1,25 +1,28 @@
-class Price:
-    def __init__(self, price):
-        self.price = price
+def get_adjecency_graph() :
+    class Price:
+        def __init__(self, price):
+            self.price = price
+
+        def __repr__(self):
+            return "price : " + str(self.price)
 
 
-flight_data = input().split()
-flight_data = [int(x) for x in flight_data]
+    flight_data = [int(x) for x in input().split()]
 
-flight_prices = []
-for i in range(0, flight_data[0]):
+    flight_prices = []
+    flight_paths = []
 
-flight_paths = []
+    adjacency_graph = [[]]
+    for i in range(0, 100):
+        adjacency_graph.append([])
 
-adjacency_graph = [[]]
-for i in range(0, 100):
-    adjacency_graph.append([])
+    for i in range(0, flight_data[0]):
+        weight = Price(int(input().split()[0]))
+        paths = [int(x) for x in input().split()]
 
-for i in range(0, flight_data[0]):
-    weight = int(input().split()[0])
-    paths = [int(x) for x in input().split()]
+        for i in range(0, len(paths) - 1):
+            adjacency_graph[paths[i]].append((paths[i + 1], weight))
 
-    for i in range(0, len(paths) - 1):
-        adjacency_graph[paths[i]].append((paths[i + 1], weight))
+    return adjacency_graph
 
-print(adjacency_graph)
+print(get_adjecency_graph())
