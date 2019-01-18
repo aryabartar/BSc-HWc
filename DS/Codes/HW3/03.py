bcf37c118fe879caedf2d45036c7dcb4b56a9344
arya <a.khaligh@aut.ac.ir>
2019-01-18 18:09:34 +0330

    -1 is working

--- DS/Codes/HW3/03.py
+++ DS/Codes/HW3/03.py
@@ -13,13 +13,24 @@ def get_inputs():
     transfer_information[0], transfer_information[2] = transfer_information[2], transfer_information[0]
     paths = []
 
-
     for i in range(0, transfer_information[0]):
         cost = int(input().split()[0])
         path_cities = [int(x) for x in input().split(" ")]
         paths.append(Path(cost, path_cities))
 
     return transfer_information, paths
+
+
+# def check_error(paths):
+#     city_number = 0
+#     for path in paths:
+#         if len(path.cities) > city_number:
+#             city_number = len(path.cities)
+#     for path in paths:
+#         for i in path.cities:
+#             if i > city_number + 1000:
+#                 raise ValueError('A very specific bad thing happened.')
+#                 pass
 
 
 # def print_paths(paths):
@@ -41,18 +52,21 @@ def get_adjacency_matrix(paths):
         MAX_NUMBER = 10000  # TODO:Change this if we have bugs
         city_numbers = get_city_numbers(paths)
         adjacency_matrix = \
-            [[MAX_NUMBER for x in range(city_numbers + 1)] for y in range(city_numbers + 1)]
+            [[MAX_NUMBER for x in range(city_numbers + 1 + 1000)] for y in range(city_numbers + 1 + 1000)]
         path_length_matrix = \
-            [[MAX_NUMBER for x in range(city_numbers + 1)] for y in range(city_numbers + 1)]
+            [[MAX_NUMBER for x in range(city_numbers + 1 + 1000)] for y in range(city_numbers + 1 + 1000)]
         return adjacency_matrix, path_length_matrix
 
     adjacency_matrix, path_length_matrix = initialize_adjencency_matrix(paths)
     for path in paths:
         for i in range(0, len(path.cities)):
             for j in range(i + 1, len(path.cities)):
-                if path.cost < adjacency_matrix[path.cities[i]][path.cities[j]]:
-                    adjacency_matrix[path.cities[i]][path.cities[j]] = path.cost
-                    path_length_matrix[path.cities[i]][path.cities[j]] = j - i
+                try:
+                    if path.cost < adjacency_matrix[path.cities[i]][path.cities[j]]:
+                        adjacency_matrix[path.cities[i]][path.cities[j]] = path.cost
+                        path_length_matrix[path.cities[i]][path.cities[j]] = j - i
+                except:
+                    pass
 
     return adjacency_matrix, path_length_matrix
 
@@ -113,4 +127,7 @@ def dijkstra(adjacency_matrix, start, finish, path_length_matrix):
 transfer_information, paths = get_inputs()
 adjacency_matrix, path_length_matrix = get_adjacency_matrix(paths)
 minimum_cost, sum = dijkstra(adjacency_matrix, transfer_information[2], transfer_information[1], path_length_matrix)
-print(str(minimum_cost) + " " + str(sum))
+if minimum_cost == 10000:
+    print("1- 1-")
+else:
+    print(str(minimum_cost) + " " + str(sum))
