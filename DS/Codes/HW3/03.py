6c27608ecd67f0a6063edc1a2b9b0131247f629d
arya <a.khaligh@aut.ac.ir>
2019-01-18 09:33:33 +0330

    Modify 03

--- DS/Codes/HW3/03.py
+++ DS/Codes/HW3/03.py
@@ -1,34 +1,18 @@
-class Price:
-    def __init__(self, price):
-        self.price = price
-
-    def __repr__(self):
-        return "price : " + str(self.price)
+class Path:
+    def __init__(self, cost, cities):
+        self.cost = cost
+        self.cities = cities
 
 
-def get_adjecency_graph(flight_data):
-    adjacency_graph = [[]]
-    for i in range(0, 100):
-        adjacency_graph.append([])
+def get_inputs():
+    transfer_information = [int(x) for x in input().split(" ")]
 
-    for i in range(0, flight_data[0]):
-        weight = Price(int(input().split()[0]))
-        paths = [int(x) for x in input().split()]
+    paths = []
+    for i in range(0, transfer_information[0]):
+        cost = int(input().split()[0])
+        paths = [int(x) for x in input().split(" ")]
 
-        for i in range(0, len(paths) - 1):
-            adjacency_graph[paths[i]].append((paths[i + 1], weight))
+        p = Path(cost, paths)
+        paths.append(p)
 
-    return adjacency_graph
-
-
-def start_dijkstra(start_node, end_node, adjacency_list):
-    def create_dijkstra_list():
-        dijkstra_list = []
-
-        for i in adjacency_list:
-            dijkstra_list.append([])
-
-
-flight_data = [int(x) for x in input().split()]
-adjacency_list = get_adjecency_graph(flight_data)
-start_dijkstra(flight_data[2], flight_data[1], adjacency_list)
+    return transfer_information, paths
