6c694fe8fb1b1f3e25475970505c33941f64ce43
arya <a.khaligh@aut.ac.ir>
2019-01-19 06:30:26 +0330

    Modify jewelry

--- DS/Codes/HW3/jewelry-new.py
+++ DS/Codes/HW3/jewelry-new.py
@@ -12,7 +12,7 @@ def get_weights():
     return weights
 
 
-def comb1(info, n, k):
+def get_different_combinations(info, n, k):
     if info.comb[n][k] > 0:
         return info.comb[n][k]
     elif k == 0:
@@ -20,8 +20,8 @@ def comb1(info, n, k):
     elif n == k:
         return 1
     else:
-        c1 = comb1(info, n - 1, k - 1)
-        c2 = comb1(info, n - 1, k)
+        c1 = get_different_combinations(info, n - 1, k - 1)
+        c2 = get_different_combinations(info, n - 1, k)
         info.comb[n][k] = c1 + c2
         return c1 + c2
 
@@ -43,27 +43,31 @@ def get_different_sums(weights):
 
 
 def divide(weights, partitions):
-    info = Information()
-    temp_partitions = 0
-    i = 0
-    while i < len(weights) - 1:
-        gp_size = 1
-
-        j = i + 1
+    def update_group_size(gp_size, j, i):
         while j < len(weights) and weights[i] == weights[j]:
             gp_size += 1
             j += 1
+        return gp_size
+
+    info = Information()
+    temp_partitions = 0
+    i = 0
+
+    while i < len(weights) - 1:
+        gp_size = 1
+        gp_size = update_group_size(gp_size, i + 1, i)
 
         asghar = get_different_sums(weights[0: i])
         for size in range(0, gp_size):
             akbar = get_different_sums(weights[i + size + 1: len(weights)])
             temp_partitions = 0
 
-            for s in range(weights[i] * (size + 1), 25000):
+            for s in range(weights[i] * (size + 1), 23000):
                 try:
-                    partitions += comb1(info, gp_size, size + 1) * \
+                    partitions += get_different_combinations(info, gp_size, size + 1) * \
                                   asghar[s - weights[i] * (size + 1)] * \
                                   akbar[s]
+
                 except:
                     try:
                         temp_partitions += asghar[s - weights[i] * (size + 1)] * \
