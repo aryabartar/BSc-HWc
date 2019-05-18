62c9a66d0290188b3aa1c3955bcddafeac7eeb3b
Arya Khaligh <bartararya@gmail.com>
2019-05-18 21:43:41 +0430

    Update Table

--- DB/HW2/Table.sql
+++ DB/HW2/Table.sql
@@ -6,3 +6,25 @@ CREATE TABLE branch(
     PRIMARY KEY (ID)
 )
 ;
+
+CREATE TABLE employee(
+    ID CHAR(5), 
+    first_name VARCHAR(128),
+    last_name VARCHAR(128), 
+    phone NUMERIC(11,0), 
+    branch_ID CHAR(5), 
+    address VARCHAR(1024),
+    PRIMARY key (ID), 
+    FOREIGN KEY (branch_ID) REFERENCES branch(ID)
+)
+;
+
+CREATE TABLE employee_box(
+    employee_ID CHAR(5), 
+    password CHAR(4), 
+    PRIMARY KEY (employee_ID),
+    FOREIGN KEY (employee_ID) REFERENCES employee(ID)
+)
+;
+
+
