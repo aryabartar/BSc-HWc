9b1ec46fa8745ad44cf9a8ff2c90a81f2b642f28
Arya Khaligh <bartararya@gmail.com>
2019-05-19 18:44:46 +0430

    Update Table.sql

--- DB/HW2/Table.sql
+++ DB/HW2/Table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE branch(
-    ID CHAR(5),
+    ID CHAR(10),
     name VARCHAR(128),
     head VARCHAR(128),
     deputy VARCHAR(128), 
@@ -8,23 +8,47 @@ CREATE TABLE branch(
 ;
 
 CREATE TABLE employee(
-    ID CHAR(5), 
+    ID CHAR(10), 
     first_name VARCHAR(128),
     last_name VARCHAR(128), 
     phone NUMERIC(11,0), 
-    branch_ID CHAR(5), 
     address VARCHAR(1024),
-    PRIMARY key (ID), 
+    branch_ID CHAR(5), 
+    PRIMARY key (ID),
     FOREIGN KEY (branch_ID) REFERENCES branch(ID)
 )
 ;
 
-CREATE TABLE employee_box(
-    employee_ID CHAR(5), 
+CREATE TABLE box(
+    employee_ID CHAR(10), 
     password CHAR(4), 
     PRIMARY KEY (employee_ID),
     FOREIGN KEY (employee_ID) REFERENCES employee(ID)
 )
 ;
 
+CREATE TABLE insurance(
+    ID CHAR(10),
+    name VARCHAR(256),
+    price NUMERIC(10,2), 
+    branch_ID CHAR(10), 
+    PRIMARY KEY (ID),
+    FOREIGN KEY (branch_ID) REFERENCES branch(ID)
+)
+;
 
+CREATE TABLE car_insurance(
+    ID CHAR(10), 
+    number_plate CHAR(7), 
+    model VARCHAR(256), 
+    color VARCHAR(256), 
+    year NUMERIC(4,0),
+    PRIMARY KEY (ID), 
+    FOREIGN KEY (ID) REFERENCES insurance(ID)
+)
+;
+
+CREATE TABLE building_insurance(
+
+)
+;
