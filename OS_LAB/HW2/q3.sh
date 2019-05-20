ed140b8208bb9d27cd3138f69efe9ef6dcf039ca
Arya Khaligh <bartararya@gmail.com>
2019-05-20 22:43:20 +0430

    Add Q3 answer

---
+++ OS_LAB/HW2/q3.sh
@@ -0,0 +1,14 @@
+INPUT1=1
+INPUT2=$INPUT1+1
+INPUT3=$INPUT2+1
+read -p "Input: " INPUT1 INPUT2 INPUT3
+
+MAX=$INPUT1
+if [ $INPUT2 -gt $INPUT1 ]; then
+    MAX=$INPUT2
+fi 
+if [ $INPUT3 -gt $INPUT2 ]; then
+    MAX=$INPUT3
+fi
+
+echo "Maximum input is: $MAX"
