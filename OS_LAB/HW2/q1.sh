aa1cb984df1a1e2d79dbf2789846450fb6bf51b4
Arya Khaligh <bartararya@gmail.com>
2019-05-20 22:25:27 +0430

    Add Q1 answer

---
+++ OS_LAB/HW2/q1.sh
@@ -0,0 +1,8 @@
+read -p "Input: " INPUT1 INPUT2
+if [ $INPUT1 -gt 10 ]; then
+    echo "Value is greater than 10!"
+elif [ $INPUT1 -gt $INPUT2 ]; then
+    echo "First number is greater than second one."
+else
+    echo "Second number is greater!"
+fi
