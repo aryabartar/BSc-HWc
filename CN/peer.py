a40d3072c275ffef05d73eed2c8ede01e2742461
Arya Khaligh <bartararya@gmail.com>
2019-05-22 23:22:28 +0430

    Fix TCP connection bug.

--- CN/peer.py
+++ CN/peer.py
@@ -11,54 +11,52 @@ def create_and_listen_on_TCP(client_UPD_address):
         """
         This method sends an UDP message to ask one client to establish a TCP connection.
         """
+        time.sleep(1)
         message = "Accept"
         print("Sendong accept!")
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(message.encode(), client_UPD_address)
+        print("client address was:", client_UPD_address)
         print("Accept sent!")
-    
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Allocates random free port and accepts request only from specified IP
-    sock.bind(('', 0)) 
+    sock.bind(('', 0))
     sock.listen(1)
     print("BB")
     while True:
         inform_client_from_server(client_UPD_address)
         print("CC")
         connection_sock, addr = sock.accept()
-        print ("Connection established with {}".format(addr))
-       
+        print("Connection established with {}".format(addr))
+
 
 def connect_to_TCP(server_address):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("EE")
     sock.connect(server_address)
-    print ("Connection with server established!")
+    print("Connection with server established!")
 
-def listen_to_UDP():
-    if sys.argv[1] == '1':
-        serverPort = 12000
-    else:
-        serverPort = 12001
 
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    serverSocket.bind(('', serverPort))
+def listen_to_UDP(sock):
 
     while True:
         print("Running listen_to_UDP while")
-        message, clientAddress = serverSocket.recvfrom(2048)
+        message, clientAddress = sock.recvfrom(2048)
         message = message.decode()
 
-        print (message)
+        print(message)
         if message == "hello":
-            print ("AA")
+            sem.acquire()
+            print("AA")
             create_and_listen_on_TCP(clientAddress)
         elif message == "Accept":
             print("DD")
-            connect_to_TCP (clientAddress)
+            connect_to_TCP(clientAddress)
+            sem.release()
 
 
-def send_UDP_broadcast():
+def send_UDP_broadcast(sock):
     serverName = '255.255.255.255'
     SEND_HELLO_INTERVAL = 2
 
@@ -67,40 +65,45 @@ def send_UDP_broadcast():
     else:
         serverPort = 12000
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock.settimeout(0.4)
-
     while True:
-        print("Running send_UDP_broadcast while")
+        sem.acquire()
+        sem.release()
 
-        time.sleep(SEND_HELLO_INTERVAL)
+        print("Running send_UDP_broadcast while")
         print(time.time())
-        
+
         message = "hello".encode()
         try:
             sock.sendto(message, (serverName, serverPort))
-        
+
         except socket.timeout:
             print("UDP hello message send timeout.\n\n")
 
+        time.sleep(SEND_HELLO_INTERVAL)
+
     sock.close()
 
-def start_send_hello_thread () :
-    interval_send_thread = threading.Thread(
-        target=listen_to_UDP, name="listen_to_UDP", args=())
-    interval_send_thread.daemon = False
-    interval_send_thread.start()
-    
 
 def create_and_run_threads():
-    rcv_UDP_thread = threading.Thread(
-        target=send_UDP_broadcast, name="send_UDP_broadcast", args=())
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
+    if sys.argv[1] == '1':
+        serverPort = 12000
+    else:
+        serverPort = 12001
+
+    sock.bind(('', serverPort))
+
+    rcv_UDP_thread = threading.Thread(
+        target=send_UDP_broadcast, name="send_UDP_broadcast", args=(sock, ))
     rcv_UDP_thread.daemon = False
     rcv_UDP_thread.start()
 
-    start_send_hello_thread()
+    interval_send_thread = threading.Thread(
+        target=listen_to_UDP, name="listen_to_UDP", args=(sock, ))
+    interval_send_thread.daemon = False
+    interval_send_thread.start()
 
 
 create_and_run_threads()
--- CN/test.py
+++ CN/test.py
@@ -1,14 +1,6 @@
 import socket
 
-serverName = '255.255.255.255'
-serverPort = 12000
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-message = "hello".encode()
-sock.sendto(message,(serverName, serverPort))
-sock.sendto(message,(serverName, 12001))
-modifiedMessage, serverAddress = sock.recvfrom(2048)
-print (modifiedMessage)
-sock.close()
+sock.bind(('', 10002))
+sock
