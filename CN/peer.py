0e9128872f904f193259896febb4a13814bd2506
Arya <bartararya@gmail.com>
2019-06-24 13:17:15 +0430

    Add wait

--- CN/peer.py
+++ CN/peer.py
@@ -101,6 +101,7 @@ def create_and_listen_on_TCP(client_UPD_address):
     TCP_sock.bind(('', 0))
     TCP_sock.listen(1)
 
+    print(TCP_sock.getsockname()[1])
     inform_client_from_server(client_UPD_address, TCP_sock.getsockname()[1])
     
     if not in_TCP_chat:
