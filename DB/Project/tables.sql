376c152e2c607a4d0b0384caae1e7329ddf300b5
Arya <bartararya@gmail.com>
2019-06-27 09:27:17 +0430

    Fix tables problems.

--- DB/Project/tables.sql
+++ DB/Project/tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE Customer (
     ssn VARCHAR(10), 
     firstname VARCHAR(10),
     lastname VARCHAR(10), 
-    customer_id VARCHAR(10), 
+    customer_id INT, 
     password VARCHAR(256), 
     PRIMARY KEY (ssn)
 );
@@ -10,19 +10,19 @@ CREATE TABLE Customer (
 CREATE TABLE PhoneNumber (
     ssn VARCHAR(10), 
     number VARCHAR(11),
-    PRIMARY KEY (ssn),
+    PRIMARY KEY (ssn, number),
     FOREIGN KEY (ssn) REFERENCES Customer(ssn)
 );
 
 CREATE TABLE Address (
     ssn VARCHAR(10), 
     address VARCHAR(11),
-    PRIMARY KEY (ssn),
+    PRIMARY KEY (ssn, address),
     FOREIGN KEY (ssn) REFERENCES Customer(ssn)
 );
 
 CREATE TABLE Account (
-    ID VARCHAR(10), 
+    ID INT AUTO_INCREMENT, 
     amount NUMERIC(10,0), 
     account_type VARCHAR(256), 
     signature_number NUMERIC(4,0), 
@@ -31,34 +31,33 @@ CREATE TABLE Account (
 );
 
 CREATE TABLE PaymentOrder (
-    ID VARCHAR(10), 
-    note VARCHAR(256),
-    account VARCHAR(10), 
+    ID INT AUTO_INCREMENT, 
+    account INT, 
     creator VARCHAR(10), 
     acceptor VARCHAR(10), 
+    note VARCHAR(1024),
     PRIMARY KEY (ID), 
     FOREIGN KEY (account) REFERENCES Account(ID), 
     FOREIGN KEY (creator) REFERENCES Customer(ssn), 
-    FOREIGN KEY (acceptor) REFERENCES Customer(ssn), 
+    FOREIGN KEY (acceptor) REFERENCES Customer(ssn)
 );
 
-CREATE TABLE transaction (
-    payment_order VARCHAR(10), 
-    destination VARCHAR(10), 
+CREATE TABLE Transaction (
+    payment_order INT, 
+    destination INT, 
     amount NUMERIC(10,0),
     PRIMARY KEY (payment_order, destination), 
-    FOREIGN KEY payment_order REFERENCES PaymentOrder(ID), 
-    FOREIGN KEY destination REFERENCES Account(ID)
-
+    FOREIGN KEY (payment_order) REFERENCES PaymentOrder(ID), 
+    FOREIGN KEY (destination) REFERENCES Account(ID)
 );
  
 CREATE TABLE Bill (
-    ID VARCHAR(10) AUTO_INCREMENT, 
-    account VARCHAR(10), 
+    ID INT AUTO_INCREMENT, 
+    account INT, 
     amount NUMERIC(10,0),
-    note VARCHAR(1024), 
     bill_type VARCHAR(256), 
     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    note VARCHAR(1024),     
     PRIMARY KEY (ID), 
     FOREIGN KEY (account) REFERENCES Account(ID), 
     CHECK (bill_type IN ("b1", "b2"))
@@ -66,50 +65,50 @@ CREATE TABLE Bill (
 
 CREATE TABLE AccountOwner (
     customer VARCHAR(10), 
-    account VARCHAR(10), 
+    account INT, 
     PRIMARY KEY (customer, account), 
-    FOREIGN KEY (customer) REFERENCES Customer(ID), 
+    FOREIGN KEY (customer) REFERENCES Customer(ssn), 
     FOREIGN KEY (account) REFERENCES Account(ID)
 );
 
 CREATE TABLE SignatureAccess (
     customer VARCHAR(10), 
-    account VARCHAR(10), 
+    account INT, 
     PRIMARY KEY (customer, account), 
-    FOREIGN KEY (customer) REFERENCES Customer(ID), 
+    FOREIGN KEY (customer) REFERENCES Customer(ssn), 
     FOREIGN KEY (account) REFERENCES Account(ID)
 );
 
 CREATE TABLE AcceptAccess (
     customer VARCHAR(10), 
-    account VARCHAR(10), 
+    account INT, 
     PRIMARY KEY (customer, account), 
-    FOREIGN KEY (customer) REFERENCES Customer(ID), 
+    FOREIGN KEY (customer) REFERENCES Customer(ssn), 
     FOREIGN KEY (account) REFERENCES Account(ID)
 );
 
 CREATE TABLE ViewAccountAccess (
     customer VARCHAR(10), 
-    account VARCHAR(10), 
+    account INT, 
     PRIMARY KEY (customer, account), 
-    FOREIGN KEY (customer) REFERENCES Customer(ID), 
+    FOREIGN KEY (customer) REFERENCES Customer(ssn), 
     FOREIGN KEY (account) REFERENCES Account(ID)
 );
 
 CREATE TABLE Settings (
     customer VARCHAR(10), 
-    account VARCHAR(10), 
+    account INT, 
     account_name VARCHAR(256), 
     color VARCHAR(256), 
     PRIMARY KEY (customer, account), 
-    FOREIGN KEY (customer) REFERENCES Customer(ID), 
+    FOREIGN KEY (customer) REFERENCES Customer(ssn), 
     FOREIGN KEY (account) REFERENCES Account(ID)
 );
 
 CREATE TABLE Signature (
     customer VARCHAR(10), 
-    payment_order VARCHAR(10), 
+    payment_order INT, 
     PRIMARY KEY (customer, payment_order), 
-    FOREIGN KEY (customer) REFERENCES Customer(ID), 
+    FOREIGN KEY (customer) REFERENCES Customer(ssn), 
     FOREIGN KEY (payment_order) REFERENCES PaymentOrder(ID)
 );
