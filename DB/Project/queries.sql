6a5661348a1f5a9e910fc9096ded6e4557c5b0ba
Arya <bartararya@gmail.com>
2019-06-28 15:06:27 +0430

    Add query2

--- DB/Project/queries.sql
+++ DB/Project/queries.sql
@@ -1,9 +1,21 @@
--- 1 
-SELECT DISTINCT Customer.firstname, Customer.lastname
-FROM Customer JOIN (
-    SELECT customer AS cust_id
-    FROM Signature 
-) AS C ON Customer.ssn = C.cust_id
-;
+-- -- 1 
+-- SELECT DISTINCT Customer.firstname, Customer.lastname
+-- FROM Customer JOIN (
+--     SELECT customer AS cust_id
+--     FROM Signature 
+-- ) AS C ON Customer.ssn = C.cust_id
+-- ;
 
--- 2
+-- -- 2
+-- SELECT *
+-- FROM Account 
+-- WHERE signature_number < (
+--     SELECT count(*)
+--     FROM SignatureAccess
+--     WHERE SignatureAccess.account = Account.ID
+-- )
+-- ;
+
+-- 3 
+SELECT * 
+FROM PaymentOrder
