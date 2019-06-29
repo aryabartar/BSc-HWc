352911ab75659e8052fd645bbaec5f94e5bf478f
Arya <bartararya@gmail.com>
2019-06-29 15:03:19 +0430

    Add AccountAndBillsInfo

--- DB/Project/queries.sql
+++ DB/Project/queries.sql
@@ -1,69 +1,70 @@
--- -- 1 
--- SELECT DISTINCT Customer.firstname, Customer.lastname
--- FROM Customer JOIN (
---     SELECT customer AS cust_id
---     FROM Signature 
--- ) AS C ON Customer.ssn = C.cust_id
--- ;
+-- 1 
+SELECT DISTINCT Customer.firstname, Customer.lastname
+FROM Customer JOIN (
+    SELECT customer AS cust_id
+    FROM Signature 
+) AS C ON Customer.ssn = C.cust_id
+;
 
--- -- 2
--- SELECT *
--- FROM Account 
--- WHERE signature_number < (
---     get_signature_access_number(Account.ID)
--- )
--- ;
+-- 2
+SELECT *
+FROM Account 
+WHERE signature_number < (
+    get_signature_access_number(Account.ID)
+)
+;
 
--- -- 3 
--- SELECT PaymentOrder.*
--- FROM PaymentOrder JOIN Account ON PaymentOrder.account = Account.ID
--- WHERE Account.signature_number <= ( 
---     get_signature_number(PaymentOrder.ID)
--- );
+-- 3 
+SELECT PaymentOrder.*
+FROM PaymentOrder JOIN Account ON PaymentOrder.account = Account.ID
+WHERE Account.signature_number <= ( 
+    get_signature_number(PaymentOrder.ID)
+);
 
--- -- 4 
--- SELECT Transaction.*
--- FROM Transaction JOIN PaymentOrder ON Transaction.payment_order = PaymentOrder.ID 
--- WHERE PaymentOrder.ID NOT IN (
---     SELECT payment_order
---     FROM AcceptPayment
--- );
+-- 4 
+SELECT Transaction.*
+FROM Transaction JOIN PaymentOrder ON Transaction.payment_order = PaymentOrder.ID 
+WHERE PaymentOrder.ID NOT IN (
+    SELECT payment_order
+    FROM AcceptPayment
+);
 
--- 5 RRR
+-- 5 RRRR
 
--- -- 6
--- -- NOTE: I considered that the shared account between two customers means both of them are owner of the same accounts.
--- SELECT * 
--- FROM Account    
--- WHERE Account.ID IN (
---     SELECT account
---     FROM AccountOwner AS A1
---     WHERE A1.customer = '1234453201' AND A1.account IN (
---         SELECT A2.account 
---         FROM AccountOwner AS A2
---         WHERE A2.customer = '1234453202'
---     )
--- );
 
--- -- 7
--- -- I assumed that a user can not sign a signed PaymentOrder.
--- SELECT 
--- FROM (SignatureAccess JOIN PaymentOrder ON SignatureAccess.account = PaymentOrder.account) 
--- WHERE SignatureAccess.customer = '1234453201' AND (SignatureAccess.customer, PaymentOrder.ID) NOT IN (
---     SELECT * 
---     FROM Signature
--- );
+-- 6
+-- NOTE: I considered that the shared account between two customers means both of them are owner of the same accounts.
+SELECT * 
+FROM Account    
+WHERE Account.ID IN (
+    SELECT account
+    FROM AccountOwner AS A1
+    WHERE A1.customer = '1234453201' AND A1.account IN (
+        SELECT A2.account 
+        FROM AccountOwner AS A2
+        WHERE A2.customer = '1234453202'
+    )
+);
 
--- -- 8
--- SELECT *
--- FROM PaymentOrder 
--- WHERE PaymentOrder.creator = '1234453202' AND PaymentOrder.creator NOT IN (
---     SELECT customer
---     FROM SignatureAccess 
---     WHERE SignatureAccess.customer = PaymentOrder.creator
--- );
+-- 7
+-- I assumed that a user can not sign a signed PaymentOrder.
+SELECT 
+FROM (SignatureAccess JOIN PaymentOrder ON SignatureAccess.account = PaymentOrder.account) 
+WHERE SignatureAccess.customer = '1234453201' AND (SignatureAccess.customer, PaymentOrder.ID) NOT IN (
+    SELECT * 
+    FROM Signature
+);
 
--- -- 9 
--- SELECT *
--- FROM Bill
--- WHERE Bill.amount > 100 AND Bill.account = 1 AND Bill.bill_type = "b1"; 
+-- 8
+SELECT *
+FROM PaymentOrder 
+WHERE PaymentOrder.creator = '1234453202' AND PaymentOrder.creator NOT IN (
+    SELECT customer
+    FROM SignatureAccess 
+    WHERE SignatureAccess.customer = PaymentOrder.creator
+);
+
+-- 9 
+SELECT *
+FROM Bill
+WHERE Bill.amount > 100 AND Bill.account = 1 AND Bill.bill_type = "b1"; 
--- DB/Project/views.sql
+++ DB/Project/views.sql
@@ -1,8 +1,15 @@
 DROP VIEW FullPaymentOrderInfo;
+DROP VIEW AccountAndBillsInfo;
 
 CREATE VIEW FullPaymentOrderInfo(ID, account, amount, creator, note) AS 
     SELECT ID, account, SUM(amount), creator, note 
     FROM PaymentOrder JOIN Transaction ON PaymentOrder.ID = Transaction.payment_order
-    GROUP BY ID; 
+    GROUP BY ID
+; 
 
-CREATE VIEW GetUserHashedPassword
+CREATE VIEW AccountAndBillsInfo(account_id, account_amount, bill_id, bill_note) AS 
+    SELECT Account.ID, Account.amount, Bill.ID, Bill.note
+    FROM Account JOIN Bill ON Account.ID = Bill.account 
+    ORDER BY Account.ID
+;
+
