eaf319d16cf4e0f21fed0de9f3589d7df1f0e6b2
Arya <bartararya@gmail.com>
2019-06-29 22:04:46 +0430

    Update get_hashed_password

--- DB/Project/functions.sql
+++ DB/Project/functions.sql
@@ -9,19 +9,18 @@ DELIMITER $$
 CREATE FUNCTION get_hashed_password(
     essn VARCHAR(10), epassword VARCHAR(256)) RETURNS VARCHAR(256)
 BEGIN
-    DECLARE password_update_time TIMESTAMP; 
+    DECLARE customer_id INT; 
     DECLARE hashed_password VARCHAR(256); 
-    DECLARE ehashed_password VARCHAR(256);
 
-    SELECT C.password_update_time INTO password_update_time
+    SELECT C.customer_id INTO password_update_time
     FROM Customer AS C
     WHERE ssn = essn; 
 
     SELECT C.password INTO hashed_password
     FROM Customer AS C
     WHERE ssn = essn; 
-    
-    SET ehashed_password = SHA2(CONCAT(essn, password_update_time, epassword), 256);
+
+    SET ehashed_password = MD5(CONCAT(customer_id, epassword));
 
     IF (ehashed_password = hashed_password) THEN 
         RETURN 1; 
