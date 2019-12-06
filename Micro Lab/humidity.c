c19de0a92187ed948b2d3c7a9e15cf2e8f247b46
Arya <bartararya@gmail.com>
2019-12-06 17:24:08 +0330

    Add humiditi code

---
+++ Micro Lab/humidity.c
@@ -0,0 +1,14 @@
+int humidityInputPin = 52; 
+
+void setup() {
+    pinMode(humidityInputPin, INPUT);
+    Serial.begin(9600);
+}
+
+void loop(){
+    int humidity = digitalRead(humidityInputPin);
+
+    Serial.print("Humidity is : "); 
+    Serial.print(humidity);
+    delay(1000);
+}
