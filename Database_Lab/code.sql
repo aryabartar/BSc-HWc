9807a4b15f0cd80646df5a540b0fd4be8880855a
Arya <bartararya@gmail.com>
2019-12-13 13:12:01 +0330

    Add Database lab project initial report.

--- Database_Lab/code.sql
+++ Database_Lab/code.sql
@@ -1,45 +1,15 @@
--- -- CREATE TABLE tb2
--- -- (
--- --  section1 text,
--- --  status1 text,
--- --  count1 INT
--- -- )
--- -- ;
+CREATE FUNCTION reffunc(refcursor) RETURNS refcursor AS '
+BEGIN
+    OPEN $1 FOR SELECT * FROM "tb1";
+    RETURN $1;
+END;
+' LANGUAGE plpgsql;
 
--- -- INSERT INTO tb2 VALUES 
--- --   ('A', 'Active', 1), ('A', 'Inactive', 2)
--- -- , ('B', 'Active', 4), ('B', 'Inactive', 5)
--- --                     , ('C', 'Inactive', 7)
--- -- ;
+BEGIN;
+SELECT reffunc('funccursor');
 
--- SELECT *
--- FROM   crosstab(
---    $$SELECT "section1", "status1", "count1"
---     FROM   tb2
---     ORDER BY 1,2$$
---    ) 
---    AS t 
---    ("section11" text, "status11" text, "count11" int)
--- ;
+FETCH 1 IN funccursor;
+MOVE FORWARD 1 FROM funccursor;
+FETCH 2 IN funccursor;
 
-CREATE TABLE tbl (
-   section   text
- , status    text
- , ct        integer 
-);
-
-INSERT INTO tbl VALUES 
-  ('A', 'Active', 1), ('A', 'Inactive', 2)
-, ('B', 'Active', 4), ('B', 'Inactive', 5)
-                    , ('C', 'Inactive', 7);  -- ('C', 'Active') is missing
-
-SELECT * 
-FROM tb1
-;
-
-SELECT *
-FROM   crosstab(
-   'SELECT section, status, ct
-    FROM   tbl
-    '  
-   ) AS ct ("Section" text, "Active" int, "Inactive" int);  
+COMMIT;
